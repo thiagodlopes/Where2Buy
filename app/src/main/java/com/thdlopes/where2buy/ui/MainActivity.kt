f657df86e3752d6d73c93c5428e42c9aa2758e03
Thiago Lopes <thiagodaniellopes99@gmail.com>
2021-07-05 15:44:50 -0300

    Adiciona dependencias

--- app/src/main/java/com/thdlopes/where2buy/ui/MainActivity.kt
+++ app/src/main/java/com/thdlopes/where2buy/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.thdlopes.where2buy
+package com.thdlopes.where2buy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thdlopes.where2buy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
